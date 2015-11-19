e878765f0dbeaaafe53e6e6ef869f0a6e84ee2d0
Dan Mouris <dgmouris@gmail.com>
2015-11-19 15:53:08 -0700

    fixed urls bug

--- stewdy/urls.py
+++ stewdy/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'^rest-auth/registration/', include('rest_auth.registration.urls'))
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'^$',include('home.urls')),
 
 )
